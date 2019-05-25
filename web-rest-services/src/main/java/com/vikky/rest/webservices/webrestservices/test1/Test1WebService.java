f6177cdcd7419adf66f7a555f0a19d85b23b9b44
Vikas <vikassingheng@gmail.com>
2019-05-25 16:49:29 -0500

    internationlization

--- web-rest-services/src/main/java/com/vikky/rest/webservices/webrestservices/test1/Test1WebService.java
+++ web-rest-services/src/main/java/com/vikky/rest/webservices/webrestservices/test1/Test1WebService.java
@@ -1,7 +1,13 @@
 package com.vikky.rest.webservices.webrestservices.test1;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class Test1WebService {
@@ -10,6 +16,24 @@ public class Test1WebService {
 		
 		SpringApplication.run(Test1WebService.class, args);
 		
+	}
+	
+	
+	@Bean 
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver lr = new SessionLocaleResolver();
+		
+		lr.setDefaultLocale(Locale.US);
+		return lr;
+	}
+	
+	@Bean
+	public ResourceBundleMessageSource messageResource() {
+	
+		ResourceBundleMessageSource rm = new ResourceBundleMessageSource();
+		rm.setBasename("messages");
+		
+		return rm;
 	}
 
 }
--- web-rest-services/src/main/java/com/vikky/rest/webservices/webrestservices/test1/UserResource.java
+++ web-rest-services/src/main/java/com/vikky/rest/webservices/webrestservices/test1/UserResource.java
@@ -1,20 +1,24 @@
 package com.vikky.rest.webservices.webrestservices.test1;
 
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
 import java.net.URI;
 import java.util.List;
+import java.util.Locale;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.mvc.ControllerLinkBuilder;
-
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -23,6 +27,9 @@ public class UserResource {
 	
 	@Autowired
 	private UserDAO service;
+	
+	@Autowired
+	private MessageSource messageSource;
 	
 	@GetMapping("/users")
 	public List<User> getAllUsers(){
@@ -67,6 +74,12 @@ public class UserResource {
 		
 		return ResponseEntity.created(uri).build();
 		
+	}
+	
+	//Internationalization
+	@GetMapping("/users/message")
+	public String getMessage(@RequestHeader(name="Accept-Language",required = false)Locale locale) {
+		return messageSource.getMessage("internation.message",null, locale);
 	}
 
 }
