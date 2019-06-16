b1cd8810fcdd3fe1d4832151eacf0917195a0f15
Vikas <vikassingheng@gmail.com>
2019-06-15 21:11:34 -0500

    microservice beinging

---
+++ limits-service/src/main/java/com/vikky/microservices/limitsservice/Configuration.java
@@ -0,0 +1,23 @@
+package com.vikky.microservices.limitsservice;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("limit-service")
+public class Configuration {
+	
+	private int minimum;
+	private int maximum;
+	public int getMinimum() {
+		return minimum;
+	}
+	public void setMinimum(int minimum) {
+		this.minimum = minimum;
+	}
+	public int getMaximum() {
+		return maximum;
+	}
+	public void setMaximum(int maximum) {
+		this.maximum = maximum;
+	}
+
+}
---
+++ limits-service/src/main/java/com/vikky/microservices/limitsservice/LimitConfig.java
@@ -0,0 +1,29 @@
+package com.vikky.microservices.limitsservice;
+
+public class LimitConfig {
+
+	private int maximum;
+	private int minimum;
+	
+	
+	public LimitConfig() {
+		super();
+	}
+	public LimitConfig(int maximum, int minimum) {
+		super();
+		this.maximum = maximum;
+		this.minimum = minimum;
+	}
+	public int getMaximum() {
+		return maximum;
+	}
+	public void setMaximum(int maximum) {
+		this.maximum = maximum;
+	}
+	public int getMinimum() {
+		return minimum;
+	}
+	public void setMinimum(int minimum) {
+		this.minimum = minimum;
+	}
+}
