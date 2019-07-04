49f7b5a3a91a48a4a99510f8ec734b5ea3183885
Vikas <vikassingheng@gmail.com>
2019-07-03 22:05:12 -0500

    with zuul

--- currency-conversion-service/src/main/java/com/vikky/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/vikky/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -7,11 +7,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 
 //@FeignClient(name="currency-exchange-service", url="localhost:8000")
-@FeignClient(name="currency-exchange-service")
+//Using Feign client to directly connect to other service
+//@FeignClient(name="currency-exchange-service")
+
+@FeignClient(name="netflix-zuul-api-gateway-server")
 @RibbonClient(name="currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 	
-	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	//@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	@GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
 	public CurrencyConversionBean getEx(@PathVariable("from") String from, @PathVariable("to") String to);
 
 }
