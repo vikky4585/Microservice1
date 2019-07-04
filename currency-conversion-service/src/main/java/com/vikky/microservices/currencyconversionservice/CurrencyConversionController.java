c676a5a6ef54383f57ce1ca8f597c0f57ef07e38
Vikas <vikassingheng@gmail.com>
2019-07-04 16:34:41 -0500

    with sleuth

--- currency-conversion-service/src/main/java/com/vikky/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/vikky/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -4,6 +4,8 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,17 +20,24 @@ public class CurrencyConversionController {
 	@Autowired
 	private CurrencyExchangeServiceProxy proxy;
 	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	
 	@GetMapping("/currency-convertor/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversionBean getEx(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
 		System.out.println("Quantity is " + quantity);
 		Map<String, String> uriVars = new HashMap<String, String>();
 		uriVars.put("to", to);
 		uriVars.put("from", from);
+		
 
 		ResponseEntity<CurrencyConversionBean> responseEntity = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", 
 				CurrencyConversionBean.class, uriVars);
 		
 		CurrencyConversionBean response = responseEntity.getBody();
+		
+		logger.info("{}", response);
+
 		return new CurrencyConversionBean(response.getId(), from, to, response.getMultiple(), quantity, quantity.multiply(response.getMultiple()), 0);
 	}
 	
@@ -36,6 +45,8 @@ public class CurrencyConversionController {
 	@GetMapping("/currency-convertor-feign/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversionBean getExFeign(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
 		CurrencyConversionBean response = this.proxy.getEx(from, to);
+		logger.info("{}", response);
+
 		return new CurrencyConversionBean(response.getId(), from, to, response.getMultiple(), quantity, quantity.multiply(response.getMultiple()), 0);
 	}
 }
--- currency-exchange-service/src/main/java/com/vikky/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ currency-exchange-service/src/main/java/com/vikky/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -3,6 +3,9 @@ package com.vikky.microservices.currencyexchangeservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,4 +15,16 @@ public class CurrencyExchangeServiceApplication {
 		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
 	}
 
+	
+	@Bean
+	public Sampler defaultSampler() {
+		return new Sampler() {
+			
+			@Override
+			public boolean isSampled(long traceId) {
+				// TODO Auto-generated method stub
+				return true;
+			}
+		};
+	}
 }
